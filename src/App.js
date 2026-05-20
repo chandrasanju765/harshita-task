b8ce4d2fc772fc21b60dab6620f9ae015eb18e98
Shalini Maurya <shalini.maurya@rapidsoft.co.in>
2026-05-20 12:34:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { lazy } from "react";
-import Dasbboard from "./Pages/Dasbboard";
-import Table from "./component/Table";
 
 const Home = lazy(() => import("./Pages/Dasbboard"));
+
 export const UserForm = lazy(() => import("./component/Modal"));
 export const Chart = lazy(() => import("./component/chart"));
 
